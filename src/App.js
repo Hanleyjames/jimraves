ee9ac1c5bd615b466be1f9d3c93c576075981877
Hanleyjames <hanley.doth@outlook.com>
2020-10-05 20:49:25 -0700

    Update frontend

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Raves Entertainment Co-op
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://readdesert.org"
           target="_blank"
           rel="noopener noreferrer"
         >
