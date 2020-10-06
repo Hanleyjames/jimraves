d25f3a05f3bd2b243035fdec5d134821ee05cc52
Hanleyjames <hanley.doth@outlook.com>
2020-10-06 11:39:15 -0700

    Edit app landing page to remove unecessary items and links from css and js file. Add font change for landing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Raves Entertainment Co-op
+          Raves Entertainment
         </p>
-        <a
-          className="App-link"
-          href="http://readdesert.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
