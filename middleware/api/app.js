5d79ccf712bd977e7f7eec5af029feccdb662c92
Hanleyjames <hanley.doth@outlook.com>
2020-10-08 13:55:54 -0700

    Set up mongo stuff

--- middleware/api/app.js
+++ middleware/api/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 //Add routes here as they are created const routename = require(path);
 const artistsRoutes = require('./routes/artists');
 const productsRoutes = require('./routes/products');
 const eventsRoutes = require('./routes/events');
 const usersRoutes = require('./routes/users');
+
+mongoose.connect('mongodb+srv://JREnt:'+process.env.MONGO_JAMES_PW+'@jimraves.6iafi.azure.mongodb.net/'+process.env.MONGO_JAMES_DB+'?retryWrites=true&w=majority');
 //use morgan during development
 //parse only integers from get requests and json
 app.use(morgan('dev'));
