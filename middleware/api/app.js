4ff921d39eeeeb7158d2b681ae635ac034cd6fb3
Hanleyjames <hanley.doth@outlook.com>
2020-10-08 14:40:05 -0700

    Add dotenv config to middleware

--- middleware/api/app.js
+++ middleware/api/app.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 //Add routes here as they are created const routename = require(path);
 const artistsRoutes = require('./routes/artists');
