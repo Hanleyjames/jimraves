f9de003d5e4f219141fe3cf67d75c485f9553360
Hanleyjames <hanley.doth@outlook.com>
2020-10-08 14:25:44 -0700

    Add environment variables for dev build or heroku build

--- middleware/api/app.js
+++ middleware/api/app.js
@@ -10,7 +10,7 @@ const productsRoutes = require('./routes/products');
 const eventsRoutes = require('./routes/events');
 const usersRoutes = require('./routes/users');
 
-mongoose.connect(STRING_URL('mongodb+srv://'+process.env.MONGO_USERNAME+process.env.MONGO_PASSWORD+'@jimraves.6iafi.azure.mongodb.net',{dbName: process.env.MONGO_DBNAME}), {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(STRING_URL('mongodb+srv://'+(process.env.MONGO_USERNAME_DEV || process.env.MONGO_USERNAME_HEROKU)+(process.env.MONGO_PASSWORD_DEV || process.env.MONGO_PASSWORD_HEROKU)+'@jimraves.6iafi.azure.mongodb.net',{dbName: process.env.MONGO_DBNAME}), {useNewUrlParser: true, useUnifiedTopology: true});
 //use morgan during development
 //parse only integers from get requests and json
 app.use(morgan('dev'));
