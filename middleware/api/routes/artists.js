c75ca05dcbcdc7d1c7156c8a9f5dee91f63776e3
Hanleyjames <hanley.doth@outlook.com>
2020-10-12 11:00:52 -0700

    Add better method response handling for get requests with id params

--- middleware/api/routes/artists.js
+++ middleware/api/routes/artists.js
@@ -48,7 +48,14 @@ router.get('/:artistID', (req, res, next) => {
     .exec()
     .then(doc => {
       console.log(doc);
-      res.status(200).json(doc);
+      if(doc){
+        res.status(200).json(doc);
+      }else{
+        res.status(404).json({
+          message: "Artist object not found",
+          id: id
+        })
+      }
     })
     .catch(err => {
       console.log(err);
