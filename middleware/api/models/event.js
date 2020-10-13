418d621970893637a6056a6d48338e54efe2a77e
Hanleyjames <hanley.doth@outlook.com>
2020-10-13 12:47:10 -0700

    Finish event model

--- middleware/api/models/event.js
+++ middleware/api/models/event.js
@@ -1,1 +1,11 @@
-//todo
+const mongoose = require('mongoose');
+
+const eventSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectID,
+  artist_ids: [mongoose.Schema.Types.ObjectID],
+  eventdatetime: DateTime,
+  venuename: String,
+  venuephone: String,
+  eventlinks: [String]
+});
+module.exports = mongoose.model('Event', eventSchema);
