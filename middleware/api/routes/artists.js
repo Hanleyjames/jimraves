0d0ed9aefd24537a6aa3f62d6f2d5f0129aff187
Hanleyjames <hanley.doth@outlook.com>
2020-10-12 19:33:32 -0700

    Add clear metadata with structure

--- middleware/api/routes/artists.js
+++ middleware/api/routes/artists.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-//Import artist model/constructor
+//Import artist model
 const Artist = require('../models/artist');
 
 router.get('/', (req, res, next) => {
@@ -11,10 +11,23 @@ router.get('/', (req, res, next) => {
     .select('_id artistname artistpicture artistbio artistlinks artistdocs')
     .exec()
     .then(docs =>{
-      //Have metadata with the retreived documents
+      //This creates a response with metadata for the retreived documents in the collection
       const response = {
         count: docs.length,
-        Artists: docs
+        Artists: docs.map(doc =>{
+          return{
+            _id: doc._id,
+            artistname: doc.artistname,
+            artistpicture: doc.artistpicture,
+            artistbio: doc.artistbio,
+            artistlinks: doc.artistlinks,
+            artistdocs: doc.artistdocs,
+            request: {
+              type: 'GET',
+              artisturl: 'http://localhost:3000/products/' + doc._id
+            }
+          }
+        })
       };
       if(docs.length > 0){
         //Return the response with status of 200 if length of the artists are over 0
@@ -77,6 +90,7 @@ router.get('/:artistID', (req, res, next) => {
   let id = req.params.artistID;
   //Call the findByID method on the Artist constructor
   Artist.findById(id)
+    .select('_id artistname artistpicture artistbio artistlinks artistdocs')
     .exec()
     .then(doc => {
       //If the document exists return the document with status code 200, or
