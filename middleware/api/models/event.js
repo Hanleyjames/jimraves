fa572af1f379c515ee5765ea635884ba864e6c63
Hanleyjames <hanley.doth@outlook.com>
2020-10-19 11:07:36 -0700

    Add relation between artists and events for models

--- middleware/api/models/event.js
+++ middleware/api/models/event.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const eventSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectID,
-  artist_ids: [mongoose.Schema.Types.ObjectID],
+  artist_ids: [{type: mongoose.Schema.Types.ObjectID, ref: 'Artist'}],
   eventdatetime:{ type: Date, default: Date.now()},
   venuename: String,
   venuephone: String,
