b2f68ad273fa38abeeededb27f5a74377d2e3a71
Hanleyjames <hanley.doth@outlook.com>
2020-10-25 21:20:53 -0700

    Get all dependencies set up for exporting methods out of the routes.

---
+++ nodeapi/api/controllers/events.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+const Event = require('../models/event');
+const Artist = require('../models/artist');
---
+++ nodeapi/api/controllers/users.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+//Import bcrypt for hasing and salting the password body request
+const bcrypt = require('bcrypt');
+// import jsonwebtoken
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+//todo
