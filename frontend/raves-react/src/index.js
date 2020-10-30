5cfa9c485591e9158d034563963d5ff100dbb281
Hanleyjames <hanley.doth@outlook.com>
2020-10-30 12:05:53 -0700

    Add folder structure to components and add partials for components

--- frontend/raves-react/src/index.js
+++ frontend/raves-react/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import Footer from './components/Footer/footer';
+import Footer from './components/_partials/Footer/footer';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
