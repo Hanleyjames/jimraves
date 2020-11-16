799ab35a44642f96631d74c522f636ac0bfa626d
Hanleyjames <hanley.doth@outlook.com>
2020-11-16 13:13:38 -0800

    Made a bs' component

---
+++ frontend/raves-react/src/components/ArtistList/ArtistList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class ArtistList extends React.component{
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+  render(){
+    return(
+      <section><p>Component Works</p></section>
+    )
+  }
+};
