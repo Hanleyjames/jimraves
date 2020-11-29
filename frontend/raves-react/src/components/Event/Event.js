629fddb6edf50da33641d5ab64dc599fb708f635
shabbamm <wbryan@protonmail.com>
2020-11-29 11:16:03 -0800

    catch me switching to linux

--- frontend/raves-react/src/components/Event/Event.js
+++ frontend/raves-react/src/components/Event/Event.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 export class Event extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = {};
+        super(props)
+
+        this.state = {
+            posts: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('https://ravesent-apiv1.herokuapp.com/artists')
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
 
     render() {
         return (
-            <section><p>Event.js works!</p></section>
+            <div>
+                
+            </div>
         )
     }
 }
