904d70568640491c0e8373f47fef9fce0848b454
Hanleyjames <hanley.doth@outlook.com>
2020-11-30 10:19:53 -0800

    Add auth-header

--- frontend/raves-react/src/services/auth-header.js
+++ frontend/raves-react/src/services/auth-header.js
@@ -2,7 +2,6 @@ export default function authHeader() {
   const token = JSON.parse(sessionStorage.getItem('token'));
 
   if (token) {
-    // for Node.js Express back-end
     return { 'x-access-token': token };
   } else {
     return {};
