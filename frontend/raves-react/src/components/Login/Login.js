df83910d32b472ec5b0162728300e8ab49c8b79a
Hanleyjames <hanley.doth@outlook.com>
2020-12-08 16:13:14 -0800

    Really buggy authentication/authorization

--- frontend/raves-react/src/components/Login/Login.js
+++ frontend/raves-react/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import { React, useState, useRef } from "react";
+import  React, { useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from 'react-router-dom';
 
