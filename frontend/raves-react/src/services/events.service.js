64b37b41cbae42f1548b5093b17b3d677130a8cb
Hanleyjames <hanley.doth@outlook.com>
2020-12-20 17:53:57 -0800

    Update event service to include rest of crud

--- frontend/raves-react/src/services/events.service.js
+++ frontend/raves-react/src/services/events.service.js
@@ -4,6 +4,27 @@ const getAll = () => {
   return http.get("/events");
 };
 
+const getOne = (id) => {
+  return http.get(`/events/${id}`);
+};
+
+const create = (data) => {
+  return http.post("/events", data);
+};
+
+const update = (id, data) => {
+  return http.put(`/events/${id}`, data);
+};
+
+const remove = (id) => {
+  return http.delete(`/events/${id}`);
+};
+
+
 export default {
-  getAll
+  getAll,
+  getOne,
+  create,
+  update,
+  remove
 };
