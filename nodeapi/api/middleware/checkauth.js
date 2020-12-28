47569b19803096f1fb80ef8882f30b74860c80c3
Hanleyjames <hanley.doth@outlook.com>
2020-12-28 13:44:58 -0800

    One more fix

--- nodeapi/api/middleware/checkauth.js
+++ nodeapi/api/middleware/checkauth.js
@@ -4,8 +4,8 @@ module.exports = (req, res, next) => {
   //Get token from headers authorization value
   try{
     console.log(req.headers);
-    console.log(req.headers.Authorization)
-    const token = req.headers.Authorization.split(" ")[1];
+    console.log(req.headers.authorization)
+    const token = req.headers.authorization.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_KEY);
     req.userData = decoded;
     next();
