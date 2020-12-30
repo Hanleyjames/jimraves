9310c385ed5e7628b94a0418d6bde122bfb2d480
Hanleyjames <hanley.doth@outlook.com>
2020-12-29 17:56:36 -0800

    Fix artist getter for basic artist (Will have to add more later)

--- frontend/raves-react/src/components/ArtistList/Artist/Artist.js
+++ frontend/raves-react/src/components/ArtistList/Artist/Artist.js
@@ -4,22 +4,34 @@ import {Link} from "react-router-dom";
 
 const Artist = (props) =>{
   const [isLoaded, setLoaded] = useState(false);
-  const [artist, setArtist] = useState([]);
+  const [artist, setArtist] = useState({});
   const [user, setUser] = useState(false);
   const [artistId] = useState(props.match.params.id);
 
   useEffect(()=>{
     isUser();
+    retrieveArtist(artistId);
   },[]);
   const isUser = () =>{
     let user = JSON.parse(sessionStorage.getItem('user'));
     if(user){
       setUser(true);
     }
-  }
+  };
+  const retrieveArtist = (id) =>{
+    ArtistsDataService.getOne(id)
+    .then(response => {
+      setArtist(response.data);
+      setLoaded(true);
+    })
+    .catch(err=>{
+      console.log(err);
+    });
+  };
   return(
     <div>
-      <p>{props.match.params.id}</p>
+      <p>{artist.artistname}</p>
+      <p>{artist.artistbio}</p>
     </div>
   )
 }
